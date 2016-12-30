a3d2eb91ab6028526cba02b310d43042aa575df4
Pulkit Kathuria <kevincobain2000@gmail.com>
2016-12-31 05:05:45 +0900

    pinterest init autolike script

---
+++ README.md
@@ -0,0 +1,12 @@
+####### Don't go crazy
+
+Pinterest allows u to like around 300 pics at a time?/day?. (so keep a track by watching console.log on chrome or just count it)
+
+####### How To
+
+Either copy paste script as it is into your developer console or install this extension https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija and paste the code for tumblr.
+
+
+###### Known Messages
+
+Sorry! You've hit a block (likes) we have in place to combat spam. Please try again later.
---
+++ script.js
@@ -0,0 +1,28 @@
+setTimeout(function(){
+    $disabled = false;
+    if (!$disabled) {
+        var r = confirm("Like Images on this Page");
+
+        if (r == true) {
+            pageScroll();
+            setInterval(function(){
+                txt = "You pressed OK!";
+
+                    elems = document.getElementsByClassName('LikeButton')
+                    var counter = 0;
+                    for(var i = 0; i< elems.length; i++) {
+                        if ( !elems[i].classList.contains("unlike") ){
+                          counter++;
+                          elems[i].click();
+                        }
+                    }
+                    console.log('liked:' + counter)
+            }, 5000);
+        }
+    }
+}, 5000);
+
+function pageScroll() {
+    window.scrollBy(0,10);
+    scrolldelay = setTimeout(pageScroll,20);
+}
