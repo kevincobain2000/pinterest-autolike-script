fa1f98b372608445f2faddde7ee44e509f517590
Pulkit Kathuria <kevincobain2000@gmail.com>
2017-01-03 19:52:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-elems = document.querySelectorAll('.LikeButton:not(.unlike)')
-for(var i = 0; i< elems.length; i++) {
-      var el = elems[i]
-      setTimeout(function(i, el){
-        if (!el.classList.contains("unlike")){
+
+setInterval(function(){
+    elems = document.querySelectorAll('.LikeButton:not(.unlike)')
+    for(var i = 0; i< elems.length; i++) {
+          var el = elems[i]
+          setTimeout(function(i, el){
             console.log(i)
             el.click();
-        }
-      }, 5000 * i, i, el);
-}
-
+          }, 5000 * i, i, el);
+    }
+}, 5000)
 
 var grid_size = 3
 setInterval(function(){
